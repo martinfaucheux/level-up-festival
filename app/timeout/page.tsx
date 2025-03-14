3e4c17de63b1185b28923e6ffc96d12ce8bdf79a
Martin Faucheux <martin.faucheux@gmail.com>
2025-03-14 21:37:17 +0100

    change countdown

--- app/timeout/page.tsx
+++ app/timeout/page.tsx
@@ -5,9 +5,13 @@ import React, { useEffect, useState } from "react";
 export default function Timeout() {
   const dateFestival = new Date("2025-09-12T18:00:00.000Z").getTime();
 
-  const remainingTime = () => {
+  const getRemainingTime = () => {
     const now = new Date().getTime();
-    const distance = dateFestival - now;
+    return dateFestival - now;
+  }
+
+  const getDigits = () => {
+    const distance = getRemainingTime();
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -15,26 +19,35 @@ export default function Timeout() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if (distance < 0) {
-      return "Le festival est commencé!";
-    }
-
-    return `${days}j ${hours}h ${minutes}m ${seconds}s`;
+    return [days, hours, minutes, seconds];
   };
 
   const [ticking, setTicking] = useState(true),
-    [remaining, setRemaining] = useState(remainingTime());
+    [remaining, setRemaining] = useState(getDigits());
 
   useEffect(() => {
     const timer = setTimeout(
-      () => ticking && setRemaining(remainingTime()),
+      () => ticking && setRemaining(getDigits()),
       1e3
     );
     return () => clearTimeout(timer);
   }, [remaining, ticking]);
+
+  const units = ["Jours", "Heures", "Minutes", "Secondes"];
+  
   return (
-    <div className="w-full text-5xl flex justify-center items-center my-5 md:my-0">
-        <div className="px-10 py-10 border-2 rounded-lg">{remaining}</div>
+    <div className="w-full text-5xl flex justify-center items-center my-5 mx-5 md:my-0">
+        <div className="flex justify-center gap-4">
+          {remaining.map((unit, index) => (
+            <div
+              key={index}
+              className="w-30 h-30 flex flex-col items-center justify-center bg-[#d66d1d] text-white rounded-full shadow-lg"
+            >
+              <span className="text-3xl font-bold">{unit}</span>
+              <span className="text-xs uppercase">{units[index]}</span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
